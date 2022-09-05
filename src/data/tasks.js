91c4b1c5e6a3ca2501859ed0bfd701252b550fd0
Fernando Irigoyen <irigoyen.retamales@gmail.com>
2022-09-05 16:19:26 -0300

    First commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primera tarea',
+        description: 'First Task'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Second Task'
+    },
+    {
+        id: 2,
+        title: 'Mi tercera tarea',
+        description: 'Third Task'
+    }
+];
